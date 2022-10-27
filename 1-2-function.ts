0ed9a898ed5db62b8c5476186442c1be568d3462
swh_95 <tjdngur22@gmail.com>
2022-10-27 11:21:41 +0900

    ADD(1-2-fuctions.ts: JS to TS about function)
    JS함수를 TS형태로 변환하는 과정 스터디

---
+++ 1-2-function.ts
@@ -0,0 +1,31 @@
+{
+    //JavaScript🤦🏻‍♀️
+    function add(num1, num2){
+        return num1 + num2;
+    }
+
+    //TypeScript
+    function add2(num1:number, num2:number): number{
+        return num1 + num2
+    }
+
+    //JavaScript🤦🏻‍♀️
+    function jsFetchNum(id) {
+        //code..
+        //code..
+        //code..
+        return new Promise((resolve, reject)=>{
+            resolve(100);
+        });
+    };
+
+    //TypeScript
+    function FetchNum(id:number):Promise<number> {
+        //code..
+        //code..
+        //code..
+        return new Promise((resolve, reject)=>{
+            resolve(100);
+        });
+    };
+}
