51528acb0fafbf5b7144448a36247d818d77990f
swh_95 <tjdngur22@gmail.com>
2022-11-02 13:26:37 +0900

    ADD(3-3-encapsulation TS-study)
    encapsulation study

---
+++ 3-oop/3-3-encapsulation.ts
@@ -0,0 +1,45 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  //public
+  //defalut 값으로 외부에서 직접적으로 접근 가능
+  //private
+  //은닉을하여 외부에서 직접적으로 접근 불가능
+  //protected
+  //현 클래스 자체에서는 건들수는 없지만 상속을 받은 자식객체에서는 접근 가능
+  class CoffeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7; //class level
+    private coffeeBeans: number = 0; //instance (object) level
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeMaker {
+      return new CoffeMaker(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("커피콩을 갯수는 0보다 커야한다.");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeMaker.BEANS_GRAMM_PER_SHOT;
+      return {
+        shots: shots,
+        hasMilk: false,
+      };
+    }
+  }
+  const maker = CoffeMaker.makeMachine(32);
+  maker.fillCoffeeBeans(32);
+}
